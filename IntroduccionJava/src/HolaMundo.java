d320c3f4fdc07315ad5350c5768fe6d24ddcc867
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-10-27 21:09:45 -0600

    inicio del proyecto

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    //funcion main (funcion principal)
+    public static void main(String[] args) {
+        System.out.println("Hola mundo con java");
+        System.out.println("Saludos");
+    } //fin del metodo main
+}
---
+++ IntroduccionJava/src/Presentacion.java
@@ -0,0 +1,7 @@
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Uriel de Jesus Atenogenes");
+        System.out.println("Edad: 17 anios");
+        System.out.println("Pais: Mexico");
+    }
+}
