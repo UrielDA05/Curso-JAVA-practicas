d615541df614ab485bc6045d032e6c73a5e69f72
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-03 16:33:59 -0600

    Concatenacion de cadenas

---
+++ Variables/src/Concatenacion.java
@@ -0,0 +1,8 @@
+public class Concatenacion {
+    public static void main(String[] args) {
+        var nombre = "Karla";
+        var apellido = "Lara";
+        var nombreCompleto = nombre + " " + apellido;
+        System.out.println("Nombre Completo = " + nombreCompleto);
+    }
+}
