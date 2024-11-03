7e90b0881996906040dc4f8643d1aef8f7b3c95c
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-02 18:51:13 -0600

    uso de variables

---
+++ Variables/src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        // variables
+        int edad = 30;
+        double sueldo = 20300.50;
+        String nombre = "Juan";
+        // Acceder a los valores
+        System.out.println("Edad: " + edad);
+        System.out.println("Sueldo: " + sueldo);
+        System.out.println("Nombre: " + nombre);
+        // Modificar valor de las variables
+        edad = 35;
+        sueldo = 380.50;
+        nombre = "Carlos";
+        System.out.println("Edad modificado: " + edad);
+        System.out.println("Sueldo modificado: " + sueldo);
+        System.out.println("Nombre: " + nombre);
+    }
+}
