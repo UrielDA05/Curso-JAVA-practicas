b2e643fde65eb3a82f7169d99941e1532a471b6b
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-15 21:58:16 -0600

    Uso de operadores

---
+++ Operadores/src/AreaRectangulo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AreaRectangulo {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.println("CAlCULO DE AREA DE RECTANGULO");
+        System.out.print("Ingrese el valor de la base: ");
+        var base = Integer.parseInt(consola.nextLine());
+        System.out.print("Ingrese el valor de la altura: ");
+        var altura = Integer.parseInt(consola.nextLine());
+        var area = 0;
+        area = base * altura;
+        System.out.println("El area del rectangulo es de: " + area);
+        var perimetro = 0;
+        perimetro = (2*base) + (2*altura);
+        System.out.println("El perimetro del rectandulo es de: " + perimetro);
+    }
+}
