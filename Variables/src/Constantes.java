b662f5f60ce59162e5b88b09ee085b7fbfdcb639
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-15 21:55:44 -0600

    Uso de constantes

---
+++ Variables/src/Constantes.java
@@ -0,0 +1,12 @@
+public class Constantes {
+    public static void main(String[] args) {
+        // Constante en Java
+        final int MI_CONSTANTE =10;
+        System.out.println("MI_CONSTANTE = " + MI_CONSTANTE);
+        // Uso de pi
+        System.out.println("Constante Math.Pi" + Math.PI);
+        // Ejemplo de constante
+        final int SEGUNDOS_POR_MINUTO = 60;
+        System.out.println("SEGUNDOS_POR_MINUTO = " + SEGUNDOS_POR_MINUTO);
+    }
+}
