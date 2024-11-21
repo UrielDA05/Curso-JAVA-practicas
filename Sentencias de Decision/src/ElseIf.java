72858743733fc3c58af7e0e00d02aa7af81dfc03
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-20 22:46:34 -0600

    Estructura selectiva de doble alternativa

---
+++ Sentencias de Decision/src/ElseIf.java
@@ -0,0 +1,12 @@
+public class ElseIf {
+    public static void main(String[] args) {
+        // Sentencias de Decicion if/Else
+        var llueve = true;
+        if (llueve){
+            System.out.println("Llevar paraguas");
+        } // Se puede quitar llaves si se cumple con una linea de codigo
+        else {
+            System.out.println("Dejar paraguas en casa");
+        }
+    }
+}
