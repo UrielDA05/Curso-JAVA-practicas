36e30ca625ac749b3d3a6398a252944d91d04de4
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-21 21:47:15 -0600

    Update AreaRectangulo.java

--- Operadores/src/AreaRectangulo.java
+++ Operadores/src/AreaRectangulo.java
@@ -4,9 +4,9 @@ public class AreaRectangulo {
     public static void main(String[] args) {
         var consola = new Scanner(System.in);
         System.out.println("CAlCULO DE AREA DE RECTANGULO");
-        System.out.print("Ingrese el valor de la base: ");
+        System.out.print("Ingrese la medida de la base: ");
         var base = Integer.parseInt(consola.nextLine());
-        System.out.print("Ingrese el valor de la altura: ");
+        System.out.print("Ingrese la medida de la altura: ");
         var altura = Integer.parseInt(consola.nextLine());
         var area = 0;
         area = base * altura;
