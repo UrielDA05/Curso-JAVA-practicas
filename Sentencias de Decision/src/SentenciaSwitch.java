df18f8e4f8539431c7d331af16c9cfeb95aa2ed7
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-25 22:26:30 -0600

    Sentencia Switch

---
+++ Sentencias de Decision/src/SentenciaSwitch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SentenciaSwitch {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.print("Proporciona un mes (1-12): ");
+        var mes = Byte.parseByte(consola.nextLine());
+        var estacion = "Esracion desconocida";
+        switch (mes){
+            case 1: case 2: case 12:
+                System.out.println("La estacion es invierno");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("La estacion es primavera");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("La estacion es verano");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("La estacion es otonio");
+                break;
+            default:
+                System.out.println(estacion);
+        }
+
+    }
+}
