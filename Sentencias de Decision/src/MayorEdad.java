8f74fd5b80e91f9c60cb4f5dae13779dc289b362
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-25 22:25:53 -0600

    Uso de If

---
+++ Sentencias de Decision/src/MayorEdad.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MayorEdad {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.print("Ingresa tu edad: ");
+        var edad = Byte.parseByte(consola.nextLine());
+        if(edad >= 18) {
+            System.out.println("La persona con " + edad + " anios, es mayor de edad");
+        }else{
+            System.out.println("La persona con " + edad + " anios, es menor de edad");
+        }
+    }
+}
