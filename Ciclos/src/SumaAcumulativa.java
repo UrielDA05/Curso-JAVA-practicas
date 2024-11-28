6dc70c77f1b144a2ed48bbc7367d6c3131cad5d9
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-27 22:16:19 -0600

    USo de For

---
+++ Ciclos/src/SumaAcumulativa.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumaAcumulativa {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.print("Ingresa el numero de la sumatoria: ");
+        var num = Integer.parseInt(consola.nextLine());
+        var acumulado = 0;
+        for (var contador = 1; contador <= num; contador++){
+            System.out.println("Acumulado de suma + numero: " + acumulado + " + " + contador);
+            acumulado += contador;
+            System.out.println("Suma de acumulado mas numero: " + acumulado);
+        }
+        System.out.println("Resultado final: " + acumulado);
+    }
+}
+// hacer con ciclo while y do-while
