1dbaececbba66921f37f246cdf21380898c48f45
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-27 22:15:38 -0600

    Ciclo for

---
+++ Ciclos/src/CicloFor.java
@@ -0,0 +1,8 @@
+public class CicloFor {
+    public static void main(String[] args) {
+        final var VALOR_MAXIMO = 5;
+        for (var contador = 1; contador <= VALOR_MAXIMO; contador++){
+            System.out.println("Valor: " + contador);
+        }
+    }
+}
