4043f7ecf7a63cb841d86682144ec993f0ba4a7f
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-28 22:41:46 -0600

    Update SumaAcumulativa.java

--- Ciclos/src/SumaAcumulativa.java
+++ Ciclos/src/SumaAcumulativa.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class SumaAcumulativa {
     public static void main(String[] args) {
         var consola = new Scanner(System.in);
-        System.out.print("Ingresa el numero de la sumatori: ");
+        System.out.print("Ingresa el numero de la sumatoria: ");
         var num = Integer.parseInt(consola.nextLine());
         var acumulado = 0;
         for (var contador = 1; contador <= num; contador++){
-            System.out.println("Acumulado de suma + numero: " + acumulado + " + " + contador);
+            System.out.println("Acumulado de la suma + numero: " + acumulado + " + " + contador);
             acumulado += contador;
             System.out.println("Suma de acumulado mas numero: " + acumulado);
         }
