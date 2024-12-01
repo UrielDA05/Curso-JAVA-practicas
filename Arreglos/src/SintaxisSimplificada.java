032298351efc911f5925403d982ae55d84d303e4
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-30 21:28:08 -0600

    IMprimir arreglos con ciclo for

--- Arreglos/src/SintaxisSimplificada.java
+++ Arreglos/src/SintaxisSimplificada.java
@@ -10,7 +10,12 @@ public class SintaxisSimplificada {
         // imprimir direccion de memoria
         System.out.println(enteros);
         System.out.println();
-
+        // uso de ciclo para la lectura de elementos
+        for (byte contador = 0; contador < enteros.length; contador++){
+            contador++;
+            System.out.printf("Valor " + contador );
+            contador--;
+            System.out.println(" con indice " + contador + ": "+ enteros[contador]);
         }
 
     }
