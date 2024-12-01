76f556791c0be9fc3ecb19759b3eeb487b3da4e4
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-30 21:09:13 -0600

    Sintaxis simplificada para la lectura

---
+++ Arreglos/src/SintaxisSimplificada.java
@@ -0,0 +1,10 @@
+public class SintaxisSimplificada {
+    public static void main(String[] args) {
+        int[] enteros = {100,200,300,400,500};
+        System.out.println("Valor 1: " + enteros[0]);
+        System.out.println("Valor 2: " + enteros[1]);
+        System.out.println("Valor 3: " + enteros[2]);
+        System.out.println("Valor 4: " + enteros[3]);
+        System.out.println("Valor 5: " + enteros[4]);
+    }
+}
