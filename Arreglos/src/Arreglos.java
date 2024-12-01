52d4f1f38f4be2aa0352594bb746a1622644fa6a
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-11-30 20:26:34 -0600

    modificacion de valores

--- Arreglos/src/Arreglos.java
+++ Arreglos/src/Arreglos.java
@@ -1,5 +1,11 @@
 public class Arreglos {
     public static void main(String[] args) {
         int[] enteros = new int[5];
+        // mopdificacion de valores
+        enteros [0] = 13;
+        enteros [1] = 21;
+        enteros [2] = 32;
+        enteros [3] = 56;
+
     }
 }
