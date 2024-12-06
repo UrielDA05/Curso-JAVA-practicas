13321761f59ec965decd137209f0a4299ff711e4
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-05 22:32:38 -0600

    Update MayorEdad.java

--- Sentencias de Decision/src/MayorEdad.java
+++ Sentencias de Decision/src/MayorEdad.java
@@ -5,10 +5,6 @@ public class MayorEdad {
         var consola = new Scanner(System.in);
         System.out.print("Ingresa tu edad: ");
         var edad = Byte.parseByte(consola.nextLine());
-        if(edad >= 18) {
-            System.out.println("La persona con " + edad + " anios, es mayor de edad");
-        }else{
-            System.out.println("La persona con " + edad + " anios, es menor de edad");
-        }
+        
     }
 }
