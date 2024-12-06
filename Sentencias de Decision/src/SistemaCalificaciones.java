3dfa63fa19102af9b10deaf3f4a14796d3c6052c
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-05 22:32:01 -0600

    Update SistemaCalificaciones.java

--- Sentencias de Decision/src/SistemaCalificaciones.java
+++ Sentencias de Decision/src/SistemaCalificaciones.java
@@ -7,7 +7,9 @@ public class SistemaCalificaciones {
         var calificacion = Float.parseFloat(consola.nextLine());
         if (calificacion >= 9 && calificacion <= 10) {
             System.out.println("Tu nota es una A");
-        
+        } else if (calificacion >= 8 && calificacion < 9) {
+            System.out.println("Tu nota es una B");
+        } else if (calificacion >= 7 && calificacion < 8) {
             System.out.println("Tu nota es una C");
         } else if (calificacion >= 6 && calificacion < 7) {
             System.out.println("Tu nota es una D");
