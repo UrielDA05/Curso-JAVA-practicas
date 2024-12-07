167ee94993d23a48b9e21919c4ae236083a34d8d
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-06 21:59:27 -0600

    Agregar elementos en arreglos

--- Arreglos/src/ValoresArreglos.java
+++ Arreglos/src/ValoresArreglos.java
@@ -1,30 +1,18 @@
 import java.util.Scanner;
 
 public class ValoresArreglos {
-    public static void main(String[] args) {
-        String materias[] = {"CalDif", "FunPro", "FunIve", "Quimic", "DesSus", "MatDis"};
-        String encabezado[] = {"ASIGNATURA", "U1", "U2", "U3", "U4", "U5"};
-        String[][] tabla = new String[materias.length + 1][encabezado.length];
-        var  consola = new Scanner(System.in);
-        for (int x = 0; x < encabezado.length; x++) {
-            tabla[0][x] = encabezado[x];
+    public static void main(String[] uriel) {
+       var consola  = new Scanner(System.in);
+        System.out.print("Proporciona el largo del arreglo: ");
+        var largoArreglo = Integer.parseInt(consola.nextLine());
+        int[] enteros = new int[largoArreglo] ;
+        for (int i = 0; i < largoArreglo; i++){
+            System.out.print("Prporciona el valor enteros[" + i + "]: ");
+            enteros[i] = Integer.parseInt(consola.nextLine());
         }
-        for (int x = 0; x < materias.length; x++) {
-            tabla[x + 1][0] = materias[x];
-        }
-        for (int x = 1; x < materias.length + 1; x++) {
-            System.out.println("Ingrese las calificaciones " + tabla[x][0] + ":");
-            for (int y = 1; y < encabezado.length; y++) {
-                System.out.print(encabezado[y] + ": ");
-                tabla[x][y] = consola.nextLine();
-            }
-        }
-        System.out.println("\nTabla de calificaciones :");
-        for (String[] fila : tabla) {
-            for (String celda : fila) {
-                System.out.print((celda != null ? celda : " ") + "   \t     ");
-            }
-            System.out.println();
+        System.out.println("\nImpresion del arreglo: ");
+        for (int i = 0; i < largoArreglo; i++){
+            System.out.println("enterso[" + i + "]: "+enteros[i]);
         }
     }
 }
