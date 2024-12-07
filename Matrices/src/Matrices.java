9b31b298bcba40bfa17b048c4ac2e1a92c6c0fe2
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-06 22:47:23 -0600

    Matrices

---
+++ Matrices/src/Matrices.java
@@ -0,0 +1,16 @@
+public class Matrices {
+    public static void main(String[] args) {
+        final int RENGLONES = 2;
+        final int COLUMNAS = 3;
+        int[][] matriz = new int[RENGLONES][COLUMNAS];
+        matriz[0][0] = 100;
+        matriz[0][1] = 200;
+        matriz[0][2] = 300;
+        matriz[1][0] = 400;
+        matriz[1][1] = 500;
+        matriz[1][2] = 600;
+        System.out.println("Valor 1 [0][0] = " + matriz [0][0]);
+        System.out.println("Valor 5 [1][1] = " + matriz [1][1]);
+        System.out.println("Valor 6 [1][2] = " + matriz [1][2]);
+    }
+}
