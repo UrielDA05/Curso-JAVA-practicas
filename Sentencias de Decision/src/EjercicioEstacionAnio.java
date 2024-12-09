41ae71cc18d65c1296bd3785d14457aeaf51c2ab
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-08 22:47:25 -0600

    Ejercicio de sentencia If

--- Sentencias de Decision/src/EjercicioEstacionAnio.java
+++ Sentencias de Decision/src/EjercicioEstacionAnio.java
@@ -11,7 +11,8 @@ public class EjercicioEstacionAnio {
             System.out.println("Estamos en primavera");
         } else if (numeroMes == 6 || numeroMes == 7 || numeroMes == 8) {
             System.out.println("Estamos en verano");
-        
+        } else if (numeroMes == 9 || numeroMes == 10 || numeroMes == 11) {
+            System.out.println("Estamos en Otonio");
         }
         else {
             System.out.println("Estacion desconocida");
