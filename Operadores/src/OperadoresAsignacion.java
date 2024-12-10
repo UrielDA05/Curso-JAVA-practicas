e00ad961ca515698078ff578deb457e19d957604
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-09 23:23:43 -0600

    Operadores de asignacion

--- Operadores/src/OperadoresAsignacion.java
+++ Operadores/src/OperadoresAsignacion.java
@@ -7,6 +7,9 @@ public class OperadoresAsignacion {
         // Asignacion compuesto
         // +=
         numero += 5; // numero = numero + 5
-        
+        System.out.println("numero = " + numero);
+        // -=, *=, /=, >=
+        numero *= 2;
+        System.out.println("numero = " + numero);
     }
 }
