aa18d677f6cf72be95f4d5d247febce647cdc399
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-13 22:23:14 -0600

    Sintaxis simplificada

--- Matrices/src/Matrices.java
+++ Matrices/src/Matrices.java
@@ -2,16 +2,12 @@ public class Matrices {
     public static void main(String[] args) {
         final int RENGLONES = 2;
         final int COLUMNAS = 3;
-        int[][] matriz = new int[RENGLONES][COLUMNAS];
-        matriz[0][0] = 100;
-        matriz[0][1] = 200;
-        matriz[0][2] = 300;
-        matriz[1][0] = 400;
-        matriz[1][1] = 500;
-        matriz[1][2] = 600;
+        // Sintaxis simplificada
+        int[][] matriz = {{100,200,300},{400,500,600}};
+
         // Recorrer los renglones
-        for (int ren = 0; ren < RENGLONES; ren++){
-            for (int col = 0; col < COLUMNAS; col++){
+        for (int ren = 0; ren < matriz.length; ren++){
+            for (int col = 0; col < matriz[ren].length; col++){
                 System.out.println("Valor["+ ren + "][" + col + "] = " + matriz[ren][col]);
             }
         }
