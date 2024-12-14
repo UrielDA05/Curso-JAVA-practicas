2d18b21584d5cc8cc141cbf7762df9955527a951
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-13 22:34:45 -0600

    Ingreso de valores en matriz

---
+++ Matrices/src/IntroducirValores.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IntroducirValores {
+    public static void main(String[] args) {
+        // Introducir valores
+        int renglones,columnas;
+        var consola = new Scanner(System.in);
+        // Definir matriz
+        System.out.print("Proporciona los renglones: ");
+        renglones = Integer.parseInt(consola.nextLine());
+        System.out.print("Proporciona el numero de columnas: ");
+        columnas = Integer.parseInt(consola.nextLine());
+        int [][] matriz = new int[renglones][columnas];
+        // solicitar valores
+        for (int ren = 0; ren < renglones; ren ++){
+            for (int col = 0; col < columnas; col ++){
+                System.out.print("Valore [" + ren + "][" + col + "]: ");
+                matriz[ren][col] = Integer.parseInt(consola.nextLine());
+            }
+        }
+    }
+}
