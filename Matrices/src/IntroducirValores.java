ae43c8378aef9ac9c30a19f636d632c3d2fabc38
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-16 23:10:19 -0600

    Ingreso de valores en matriz

--- Matrices/src/IntroducirValores.java
+++ Matrices/src/IntroducirValores.java
@@ -18,13 +18,6 @@ public class IntroducirValores {
                 matriz[ren][col] = Integer.parseInt(consola.nextLine());
             }
         }
-        // iterar valores en matriz
-        System.out.println();
-        for (int ren = 0; ren < renglones; ren ++) {
-            for (int col = 0; col < columnas; col++) {
-                System.out.println("Matriz [" + ren + "][" + col + "] = "
-                        + matriz[ren][col] + " ");
-            }
-        }
+
     }
 }
