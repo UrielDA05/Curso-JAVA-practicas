97b44ecf70c1d3b9b2e1de5d69b7ef1ec2935a4f
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-17 21:43:38 -0600

    Suma de valores

--- Matrices/src/SumaDiagonalMatriz.java
+++ Matrices/src/SumaDiagonalMatriz.java
@@ -7,8 +7,8 @@ public class SumaDiagonalMatriz {
         columnas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de columnas"));
         int matriz[][] = new int[filas][columnas];
         if ( filas == columnas){
-            for (int a = 1; a < filas; a++){
-                for (int b = 1; b < columnas; b++){
+            for (int a = 0; a < filas; a++){
+                for (int b = 0; b < columnas; b++){
                     matriz[a][b] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valo para " +
                             a+","+b));
                 }
