e200b70a6cec767856d8c1a7b2dd9be5d945084a
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-17 21:31:17 -0600

    Solicitud de valores

--- Matrices/src/SumaDiagonalMatriz.java
+++ Matrices/src/SumaDiagonalMatriz.java
@@ -6,7 +6,17 @@ public class SumaDiagonalMatriz {
         filas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de filas"));
         columnas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de columnas"));
         int matriz[][] = new int[filas][columnas];
+        if ( filas == columnas){
+            for (int a = 1; a < filas; a++){
+                for (int b = 1; b < columnas; b++){
+                    matriz[a][b] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valo para " +
+                            a+","+b));
+                }
+            }
 
+        }else {
+            JOptionPane.showInputDialog("Ingrese valores iguales");
+        }
 
     }
 }
