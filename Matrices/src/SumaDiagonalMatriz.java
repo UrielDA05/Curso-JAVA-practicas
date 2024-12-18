5224f2d710d869be35d9ef08be2d48b1718e6964
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-17 21:25:56 -0600

    Solicitud del tamanio de la raiz

---
+++ Matrices/src/SumaDiagonalMatriz.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class SumaDiagonalMatriz {
+    public static void main(String[] args) {
+        int filas,columnas;
+
+        filas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de filas"));
+        columnas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de columnas"));
+        int matriz[][] = new int[filas][columnas];
+
+
+    }
+}
