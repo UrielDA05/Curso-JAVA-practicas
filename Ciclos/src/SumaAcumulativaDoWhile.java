f91cd7c9fbd2c9ca015b2f98d79158d2ecf8cbc0
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-21 22:47:55 -0600

    Update SumaAcumulativaDoWhile.java

--- Ciclos/src/SumaAcumulativaDoWhile.java
+++ Ciclos/src/SumaAcumulativaDoWhile.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class SumaAcumulativaDoWhile {
     public static void main(String[] args) {
-        var consola = new Scanner(System.in);
-        System.out.print("Ingresa el numero de la sumatoria: ");
-        var num = Integer.parseInt(consola.nextLine());
-        var acumulado = 0;
-        var contador = 1;
+        
         do {
             System.out.println("Acumulado de suma + numero: " + acumulado + " + " + contador);
             acumulado += contador;
