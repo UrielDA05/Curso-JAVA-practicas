be900689ede68b76fa11116b7cbe96b2ab9181e0
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-24 23:52:23 -0600

    Uso de operador ternario

--- Sentencias de Decision/src/OperadorTernario.java
+++ Sentencias de Decision/src/OperadorTernario.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class OperadorTernario {
     public static void main(String[] args) {
         var consola = new Scanner(System.in);
-        System.out.print("Ingrese el primer numero: ");
+        System.out.print("Ingrese un primer numero: ");
         var num1 = Integer.parseInt(consola.nextLine());
-        System.out.print("Ingrese el segundo numero");
+        System.out.print("Ingrese un segundo numero");
         var num2 = Integer.parseInt(consola.nextLine());
         System.out.println( num1 >num2 ? num1 : num2);
     }
