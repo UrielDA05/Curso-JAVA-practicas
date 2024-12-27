e903bd42480c3fd02eebdbb568ecb7a0d2b5ef2c
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-26 22:24:32 -0600

    Update AreaRectangulo.java

--- Operadores/src/AreaRectangulo.java
+++ Operadores/src/AreaRectangulo.java
@@ -11,8 +11,7 @@ public class AreaRectangulo {
         var area = 0;
         area = base * altura;
         System.out.println("El area del rectangulo es de: " + area);
-        var perimetro = 0;
-        perimetro = 2(base + altura)
+        
         System.out.println("El perimetro del rectandulo es de: " + perimetro);
     }
 }
