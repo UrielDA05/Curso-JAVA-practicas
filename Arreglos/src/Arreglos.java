937065efbc9f41dd8bb67cf72f4effe32aa41798
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-28 23:25:41 -0600

    Update Arreglos.java

--- Arreglos/src/Arreglos.java
+++ Arreglos/src/Arreglos.java
@@ -8,7 +8,6 @@ public class Arreglos {
         enteros [3] = 56;
         // lectura de valores en los arreglos
         System.out.println("Valor 1 = " + enteros[0]);
-        // Al no cambiar el valor se queda en 0
-        System.out.println("Valor 5 = " + enteros[4]);
+        
     }
 }
