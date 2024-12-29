6476c0778757581af642c82d537ccd769de9efc5
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-28 23:25:22 -0600

    Update Arreglos.java

--- Arreglos/src/Arreglos.java
+++ Arreglos/src/Arreglos.java
@@ -2,7 +2,10 @@ public class Arreglos {
     public static void main(String[] args) {
         int[] enteros = new int[5];
         // mopdificacion de valores
-        
+        enteros [0] = 13;
+        enteros [1] = 21;
+        enteros [2] = 32;
+        enteros [3] = 56;
         // lectura de valores en los arreglos
         System.out.println("Valor 1 = " + enteros[0]);
         // Al no cambiar el valor se queda en 0
