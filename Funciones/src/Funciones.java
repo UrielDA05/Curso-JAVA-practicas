ddfbf0a01805ba166c51631734c0c34358ad49fa
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-29 22:57:10 -0600

    Funciones en Java

---
+++ Funciones/src/Funciones.java
@@ -0,0 +1,11 @@
+public class Funciones {
+    // Definir la funcion saludar
+    static void saludar(String mensaje){
+        System.out.println("Mensaje: " + mensaje);
+    }
+
+    public static void main(String[] args) {
+        saludar("Hola");
+        saludar("Adios");
+    }
+}
