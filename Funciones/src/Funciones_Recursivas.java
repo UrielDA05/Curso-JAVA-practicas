bc8eeedc88063025ce072e789d46f2d95ddd742d
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-30 23:08:21 -0600

    Uso de la funcion recursiva

---
+++ Funciones/src/Funciones_Recursivas.java
@@ -0,0 +1,14 @@
+public class Funciones_Recursivas {
+    // IMprimir numeros del 5 al 1
+
+    // Definir funcion recursiva
+    static void funcionRecursiva(int numero){
+        // Caso base
+        if (numero = 1)
+            System.out.println(numero);
+        else{
+            // Caso recursivo
+            System.out.println(numero);
+            funcionRecursiva(numero - 1);
+    }
+}
