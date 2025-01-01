1acaff451870cea69ccdde8fd119e110b1441baa
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-31 22:38:50 -0600

    Ejercicio de funciones

--- Funciones/src/Numero_Par.java
+++ Funciones/src/Numero_Par.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Numero_Par {
     // Funcion para saber si un numero es par
     static boolean esPar(int numero){
-        //if(numero % 2 == 0)
-      //      return true;
-    //    else
-  //          return false;
+        if(numero % 2 == 0)
+           return true;
+        else
+           return false;
        // return (numero % 2 == 0)? true : false;
     }
     public static void main(String[] args) {
