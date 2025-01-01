952c626c109fea5664c1f2254429f11a22e49152
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-01 16:14:14 -0600

    Ejercicio de funciones

--- Funciones/src/Numero_Par.java
+++ Funciones/src/Numero_Par.java
@@ -10,7 +10,7 @@ public class Numero_Par {
        // return (numero % 2 == 0)? true : false;
     }
     public static void main(String[] args) {
-        System.out.print("Proporciona un valor entero: ");
+        System.out.print("Proporciona un número entero: ");
         var numero = Integer.parseInt(new Scanner(System.in).nextLine());
         System.out.println("Numero par? " + esPar(numero));
     }
