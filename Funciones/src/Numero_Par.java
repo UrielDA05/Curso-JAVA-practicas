1ad6036b1e29a4b55a952cf66381a44daf1e8dae
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2024-12-31 22:37:53 -0600

    Update Numero_Par.java

--- Funciones/src/Numero_Par.java
+++ Funciones/src/Numero_Par.java
@@ -2,13 +2,7 @@ import java.util.Scanner;
 
 public class Numero_Par {
     // Funcion para saber si un numero es par
-    static boolean esPar(int numero){
-        //if(numero % 2 == 0)
-      //      return true;
-    //    else
-  //          return false;
-        return (numero % 2 == 0)? true : false;
-    }
+  
     public static void main(String[] args) {
         System.out.print("Proporciona un valor entero: ");
         var numero = Integer.parseInt(new Scanner(System.in).nextLine());
