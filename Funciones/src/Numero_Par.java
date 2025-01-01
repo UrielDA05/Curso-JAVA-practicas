30a8beac635855cfd9b46330f98bfe9293d5a395
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-01 16:15:27 -0600

    Ejercicio de funciones

--- Funciones/src/Numero_Par.java
+++ Funciones/src/Numero_Par.java
@@ -8,6 +8,7 @@ public class Numero_Par {
         else
            return false;
        // return (numero % 2 == 0)? true : false;
+        
     }
     public static void main(String[] args) {
         System.out.print("Proporciona un número entero: ");
