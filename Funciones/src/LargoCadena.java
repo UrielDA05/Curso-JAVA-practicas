612531da85c9ccb2a3577a5c5959b3e1579de5fa
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-02 22:09:39 -0600

    Obtener el largo de una cadena

--- Funciones/src/LargoCadena.java
+++ Funciones/src/LargoCadena.java
@@ -6,7 +6,13 @@ public class LargoCadena {
 
         // Recorrer la cadena
         for (int i = 0; i < cadena.length(); i++){
-            System.out.println(i + " " cadena.charAt(i));
+            System.out.println(i + " " + cadena.charAt(i));
         }
+        // Subcadenas
+        // substring (indice_inicio, indice_fin)
+        var subcadena1 = cadena.substring(0,4);
+        var subcadena2 = cadena.substring(5,10);
+        System.out.println("Subcadena Hola" + subcadena1);
+        System.out.println(subcadena2);
     }
 }
