6dc6ece9d6db6f1f813099167642751c49cc3849
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-02 22:01:22 -0600

    Obtener el largo de una cadena

--- Funciones/src/LargoCadena.java
+++ Funciones/src/LargoCadena.java
@@ -2,6 +2,6 @@ public class LargoCadena {
     public static void main(String[] args) {
         // Ejemplo obtener largo de una cadena
         var cadena = "Hola Mundo";
-        System.out.println(cadena.length());
+        System.out.println("Largo de la cadena: " + cadena.length());
     }
 }
