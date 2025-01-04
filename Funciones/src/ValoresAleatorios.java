db893e1c505ccf8fe239985ca6b1d56b56a92a91
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-03 22:27:19 -0600

    Funcion de valor aleatorio

---
+++ Funciones/src/ValoresAleatorios.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class ValoresAleatorios {
+    public static void main(String[] args) {
+        // Funcion Numeros Aleatorios
+        var aleatorio = new Random();
+        // Generar valores entre 0 y 100 (no se incluye el valor limite)
+    }
+}
