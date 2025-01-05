793607a8dc004b4641d7c4eaa65a0d6f7a8cea9e
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-04 22:30:45 -0600

    Funcion de redondeo de numeros

--- Funciones/src/FuncionRedondeo.java
+++ Funciones/src/FuncionRedondeo.java
@@ -4,11 +4,12 @@ public class FuncionRedondeo {
     public static void main(String[] args) {
         // Redondeo
         // Mayor a .5 redondea hacia arriba
-        var numero = 8.55;
+        var numero = 8.5555;
         var redondeo = Math.round(numero);
         System.out.println("Valor " + numero + " redondeado: " + redondeo);
         // Formato
         var patron = "#.##";
+        //var patron = "#";
         var decimalFormat = new DecimalFormat(patron);
         var numeroFormato = decimalFormat.format(numero);
         System.out.println("Valor " + numero + " con formato: " + numeroFormato);
