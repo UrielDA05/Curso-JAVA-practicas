67f79aaf84d162c61fff3c9139a46f5106c16112
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-05 23:10:24 -0600

    Update ValoresAleatorios.java

--- Funciones/src/ValoresAleatorios.java
+++ Funciones/src/ValoresAleatorios.java
@@ -8,7 +8,6 @@ public class ValoresAleatorios {
         var valorAleatorio = aleatorio.nextInt(0,100+1);
         System.out.println("Valor aleatorio (0 y 100): " + valorAleatorio);
         // Generar valor aleatorio entre 100 y 200
-        var valorAleatorio2 = aleatorio.nextInt(100,200+1);
-        System.out.println("Valor aleatorio (100 y 200): " + valorAleatorio2);
+        
     }
 }
