450629fc06ea11179dd42980d70b3f35dd7c7290
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-09 20:57:44 -0600

    Ejercicio de generador de un ID

--- Funciones/src/GeneradorID.java
+++ Funciones/src/GeneradorID.java
@@ -9,7 +9,9 @@ public class GeneradorID {
         nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
         apellido = JOptionPane.showInputDialog("Ingresa tu apellido");
         anioNacimineto =Short.parseShort(JOptionPane.showInputDialog("Ingresa tu anio de nacimiento"));
-
+        // Extraccion de caracteres
+        var subcadena1 = nombre.substring(0,1);
+        System.out.println(subcadena1);
 
     }
 }
