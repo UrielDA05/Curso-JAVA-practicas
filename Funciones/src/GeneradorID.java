9e6c25155e57b5340af6fd7d4d63f48d2b29b307
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-09 21:06:38 -0600

    Ejercicio para generar un ID unico

--- Funciones/src/GeneradorID.java
+++ Funciones/src/GeneradorID.java
@@ -5,13 +5,21 @@ public class GeneradorID {
         // Declaracion de variables
         String nombre, apellido;
         short anioNacimineto;
+
         // Solicitud de valores
         nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
         apellido = JOptionPane.showInputDialog("Ingresa tu apellido");
         anioNacimineto =Short.parseShort(JOptionPane.showInputDialog("Ingresa tu anio de nacimiento"));
+
+        // Pasar datos a mayusculas
+        nombre = nombre.toUpperCase();
+        apellido = apellido.toUpperCase();
+
         // Extraccion de caracteres
-        var subcadena1 = nombre.substring(0,1);
-        System.out.println(subcadena1);
+        var subcadena1 = nombre.substring(0,2);
+        var subcadena2 = apellido.substring(0,2);
+        
+        //System.out.println(subcadena1);
 
     }
 }
