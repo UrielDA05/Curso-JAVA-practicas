3acc90d350b9aa3337ca98277844f28f58031c47
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-19 22:56:58 -0600

    Ejercicio de funciones

--- Funciones/src/GeneradorID.java
+++ Funciones/src/GeneradorID.java
@@ -4,33 +4,38 @@ import java.util.Random;
 
 public class GeneradorID {
     public static void main(String[] args) {
-        generadorID();
+
+        generadorID obj1 = new generadorID();
+        obj1.ingresar();
     }
-    public void generadorID(){
-        // Declaracion de variables
-        String nombre, apellido;
-        short anioNacimineto;
-        var aleatoirio = new Random();
-        var formato = "# # # #";
-        var decimalFormat = new DecimalFormat(formato);
+public class generadorID{
+        static void ingresar(){
+            // Declaracion de variables
+            String nombre, apellido;
+            short anioNacimineto;
+            var aleatoirio = new Random();
+            var formato = "# # # #";
+            var decimalFormat = new DecimalFormat(formato);
 
-        // Solicitud de valores
-        nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
-        apellido = JOptionPane.showInputDialog("Ingresa tu apellido");
-        anioNacimineto =Short.parseShort(JOptionPane.showInputDialog("Ingresa tu anio de nacimiento"));
+            // Solicitud de valores
+            nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
+            apellido = JOptionPane.showInputDialog("Ingresa tu apellido");
+            anioNacimineto =Short.parseShort(JOptionPane.showInputDialog("Ingresa tu anio de nacimiento"));
 
-        // Pasar datos a mayusculas
-        nombre = nombre.toUpperCase();
-        apellido = apellido.toUpperCase();
+            // Pasar datos a mayusculas
+            nombre = nombre.toUpperCase();
+            apellido = apellido.toUpperCase();
 
-        // Extraccion de caracteres
-        var subcadena1 = nombre.substring(0,2);
-        var subcadena2 = apellido.substring(0,2);
+            // Extraccion de caracteres
+            var subcadena1 = nombre.substring(0,2);
+            var subcadena2 = apellido.substring(0,2);
 
-        // Valores aleatorios
-        var valorAleatorio = aleatoirio.nextInt(0,10000);
-        var valorAleatorioFor = decimalFormat.format(valorAleatorio);
+            // Valores aleatorios
+            var valorAleatorio = aleatoirio.nextInt(0,10000);
+            var valorAleatorioFor = decimalFormat.format(valorAleatorio);
 
-        JOptionPane.showMessageDialog(null,"Su ID es: " +subcadena1 + subcadena2 + anioNacimineto + valorAleatorioFor );
+            JOptionPane.showMessageDialog(null,"Su ID es: " +subcadena1 + subcadena2 + anioNacimineto + valorAleatorioFor );
+        }
+
     }
 }
