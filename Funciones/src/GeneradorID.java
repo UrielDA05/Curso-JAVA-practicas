03f126fda6d2994e06512d1023209245832719b1
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-19 22:49:13 -0600

    Ejercicio de funciones

--- Funciones/src/GeneradorID.java
+++ Funciones/src/GeneradorID.java
@@ -7,6 +7,30 @@ public class GeneradorID {
         generadorID();
     }
     public void generadorID(){
+        // Declaracion de variables
+        String nombre, apellido;
+        short anioNacimineto;
+        var aleatoirio = new Random();
+        var formato = "# # # #";
+        var decimalFormat = new DecimalFormat(formato);
 
+        // Solicitud de valores
+        nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
+        apellido = JOptionPane.showInputDialog("Ingresa tu apellido");
+        anioNacimineto =Short.parseShort(JOptionPane.showInputDialog("Ingresa tu anio de nacimiento"));
+
+        // Pasar datos a mayusculas
+        nombre = nombre.toUpperCase();
+        apellido = apellido.toUpperCase();
+
+        // Extraccion de caracteres
+        var subcadena1 = nombre.substring(0,2);
+        var subcadena2 = apellido.substring(0,2);
+
+        // Valores aleatorios
+        var valorAleatorio = aleatoirio.nextInt(0,10000);
+        var valorAleatorioFor = decimalFormat.format(valorAleatorio);
+
+        JOptionPane.showMessageDialog(null,"Su ID es: " +subcadena1 + subcadena2 + anioNacimineto + valorAleatorioFor );
     }
 }
