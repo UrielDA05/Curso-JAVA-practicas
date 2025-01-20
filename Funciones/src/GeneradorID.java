8d8fad86f00dc569d6c874c776d648ceae2403f4
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-19 23:07:58 -0600

    Ejercicio de funciones

--- Funciones/src/GeneradorID.java
+++ Funciones/src/GeneradorID.java
@@ -33,7 +33,7 @@ public class GeneradorID {
             var valorAleatorio = aleatoirio.nextInt(0,10000);
             var valorAleatorioFor = decimalFormat.format(valorAleatorio);
             var id = subcadena1 + subcadena2 + anioNacimineto + valorAleatorioFor;
-            JOptionPane.showMessageDialog(null,"Su ID es: " + );
+            JOptionPane.showMessageDialog(null,"Su ID es: " + id );
         }
 
     }
