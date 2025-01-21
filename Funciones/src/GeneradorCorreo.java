4d742a6271eb1406b48df994b86318555b15cb0e
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-20 22:56:16 -0600

    Ejercicio de funciones

--- Funciones/src/GeneradorCorreo.java
+++ Funciones/src/GeneradorCorreo.java
@@ -4,15 +4,17 @@ public class GeneradorCorreo {
     public static void main(String[] args) {
         correo();
     }
-    static void correo(){
+    static void correo() {
         // Solicitud de nombre
         var nombre = JOptionPane.showInputDialog("Ingresa tu nombre").toLowerCase();
 
         // Solicitud de apellido
         var apellido = JOptionPane.showInputDialog("Ingresa tu apellido").toLowerCase();
 
-        //
+        // Dirreccion
+        var dir = "@gmail.com";
+
         // Concatenacion
-        var cadena = nombre + '.' + apellido +
+        var cadena = nombre + '.' + apellido + dir;
     }
 }
