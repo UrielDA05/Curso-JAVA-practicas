6735986904fe569359ab5cae383e782dd650e968
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-21 22:43:36 -0600

    Ejercicio de funciones

--- Funciones/src/GeneradorID.java
+++ Funciones/src/GeneradorID.java
@@ -8,8 +8,14 @@ public class GeneradorID {
         generadorID();
     }
         static void generadorID(){
+            // Declaracion de variables
+            String nombre, apellido;
+            short anioNacimineto;
+            var aleatoirio = new Random();
+            var formato = "# # # #";
+            var decimalFormat = new DecimalFormat(formato);
 
-        }
+
 
     }
 
