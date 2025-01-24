0a4dff0afe1305a7f5d3787e8256fa554ffda8ba
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-23 21:09:03 -0600

    Clases y objetos

--- Clases/src/Aritmetica.java
+++ Clases/src/Aritmetica.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Aritmetica {
     int operando1;
     int operando2;
@@ -17,12 +19,23 @@ public class Aritmetica {
         System.out.println("Resultado de la multiplicacion: " + resultado);
     }
 
-    void dividir{
+    void dividir(){
         var resultado = operando1 / operando2;
         System.out.println("Resultado de la division: " + resultado);
     }
 
     public static void main(String[] args) {
+        var seleccion = JOptionPane.showInputDialog("""
+                LISTA DE OPERACIONES
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division\t
+                Ingrese el numero de la operacion a realizar""");
+        switch (seleccion){
+            case 1:
+
+        }
 
     }
 }
