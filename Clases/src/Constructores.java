53ddc49c637f2b18bf153bc4e0a534c3ff7e02d9
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-26 21:49:09 -0600

    Uso de Constructores

--- Clases/src/Constructores.java
+++ Clases/src/Constructores.java
@@ -1,9 +1,30 @@
+import javax.swing.*;
+
 public class Constructores {
     int operando1;
     int operando2;
     public Constructores(int op1, int op2){
         operando1 = op1;
         operando2 = op2;
+    }
+    void sumar(){
+        var resultado = operando1 + operando2;
+        JOptionPane.showMessageDialog(null,"Resultado de la suma: " + resultado);
+    }
+
+    void restar(){
+        var resultado = operando1 - operando2;
+        JOptionPane.showMessageDialog(null,"Resultado de la resta: " + resultado);
+    }
+
+    void multiplicar(){
+        var resultado = operando1 * operando2;
+        JOptionPane.showMessageDialog(null,"Resultado de la multiplicacion: " + resultado);
+    }
+
+    void dividir(){
+        var resultado = operando1 / operando2;
+        JOptionPane.showMessageDialog(null,"Resultado de la division: " + resultado);
     }
 }
 
