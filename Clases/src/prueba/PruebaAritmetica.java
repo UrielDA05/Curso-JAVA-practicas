b44b39c803f70a554a371b8f124e93d77c01632c
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-28 12:40:48 -0600

    Prueba en distintos paquetes

--- Clases/src/prueba/PruebaAritmetica.java
+++ Clases/src/prueba/PruebaAritmetica.java
@@ -1,4 +1,16 @@
 package prueba;
 
+import aritmetica.Constructores;
+
+import javax.swing.*;
+
 public class PruebaAritmetica {
+    public static void main(String[] args) {
+
+        var operando = new Constructores(9,8);
+        operando.multiplicar();
+        JOptionPane.showMessageDialog(null,"Direccion de memoria: " + operando);
+        var operando2 = new Constructores(12,16);
+        JOptionPane.showMessageDialog(null,"Dir. memoraia obj2: " + operando2);
+    }
 }
