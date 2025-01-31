d6d9764d02e5e86eacd875cea2a3ddfc6767564e
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-01-30 21:24:46 -0600

    Update Constructores.java

--- Clases/src/aritmetica/Constructores.java
+++ Clases/src/aritmetica/Constructores.java
@@ -5,10 +5,7 @@ public class Constructores {
     int operando1;
     int operando2;
 
-    // Constructor con parametros
-    public Constructores(int operando1, int operando2){
-        this.operando1 = operando1;
-        this.operando2 = operando2;
+    
         
     }
     public void sumar(){
