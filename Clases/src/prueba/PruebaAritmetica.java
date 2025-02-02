a8a573c4b87981560fab4825e752f93094bb8b60
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-01 22:05:32 -0600

    Prueba en distintos paquetes

--- Clases/src/prueba/PruebaAritmetica.java
+++ Clases/src/prueba/PruebaAritmetica.java
@@ -1,10 +1,10 @@
 package prueba;
-import aritmetica.Constructores;
+import aritmetica.Aritmetica;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
 
-        var operando = new Constructores(9,8);
+        var operando = new Aritmetica(9,8);
         operando.multiplicar();
         var operando2 = new Constructores(12,16);
         operando2.sumar();
