f783e84ecdcbf1d2225e9c89dfe2d73bec535b8a
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-03 22:40:48 -0600

    Ejemplo de encapsulamiento

--- Clases/src/prueba/PruebaAritmetica.java
+++ Clases/src/prueba/PruebaAritmetica.java
@@ -7,6 +7,8 @@ public class PruebaAritmetica {
         var operando = new Aritmetica(9,8);
         operando.getOperando1(); // 9
         System.out.println("Valor de operando 1: " + operando.getOperando1());
-
+        operando.setOperando1(5);
+        System.out.println("Valor nuevo: " + operando.getOperando1());
+        operando.sumar();
     }
 }
