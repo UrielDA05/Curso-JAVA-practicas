8891a2ebc012ae1c6e29632578f761fc58c65251
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-04 21:10:51 -0600

    Ejemplo de encapsulamiento

--- Clases/src/persona/Persona.java
+++ Clases/src/persona/Persona.java
@@ -8,17 +8,10 @@ public class Persona {
         this.nombre = nombre;
         this.apellido = apellido;
     }
-
-    public static void main(String[] args) {
-        // Creacion de objetos de la clase persona
-        Persona obj1 = new Persona();
-        obj1.nombre = "Uriel";
-        obj1.apellido = "Atenogenes";
-        obj1.mostrarPersona();
-        
-        var obj2 = new Persona();
-        obj2.nombre = "Layla";
-        obj2.apellido = "Acosta";
-        obj2.mostrarPersona();
+    public void getNombre(){
+        return this.nombre;
+    }
+    public void getApellido(){
+        return this.apellido ;
     }
 }
