d4de1d44abff8aa5aebf65125b774634b8f3febb
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-05 22:51:24 -0600

    Ejemplo de encapsulamiento

--- Clases/src/persona/Persona.java
+++ Clases/src/persona/Persona.java
@@ -1,16 +1,18 @@
 package persona;
 
+import javax.swing.*;
+
 public class Persona {
     private String nombre;
     private String apellido;
 
     void mostrarNombre(String nombre){
         this.nombre = nombre;
-
+        nombre = JOptionPane.showInputDialog("Ingrese su nombre");
     }
     void mostrarApellido(String apellido){
         this.apellido = apellido;
-
+        apellido = JOptionPane.showInputDialog("Ingrese su apellido");
     }
     
     public String getNombre(){
