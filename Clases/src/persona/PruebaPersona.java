807d821a0f2abe5fb4e6d060245bb8e39f2c1fe8
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-07 21:15:43 -0600

    Ejemplo de encapsulamiento

--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -1,11 +1,16 @@
 package persona;
 
+import javax.swing.*;
+
 public class PruebaPersona {
     public static void main(String[] args) {
         var nom = new Persona();
-        nom.setNombre("Uriel");
+        var n = JOptionPane.showInputDialog("Ingrese su nombre");
+        nom.setNombre(n);
+
         var ape = new Persona();
-        ape.setApellido("Atenogenes");
+        var a = JOptionPane.showInputDialog("Ingrese su apellido");
+        ape.setApellido(a);
         var mos = new Persona();
         mos.mostrar();
     }
