138f7db1a8dda477dfeac8e59bc1d9fe898ddbbd
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-09 19:03:44 -0600

    Ejemplo de encapsulamiento

--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -1,17 +1,13 @@
 package persona;
-
 import javax.swing.JOptionPane;
 
 public class PruebaPersona {
     public static void main(String[] args) {
-        var nom = new Persona();
         var n = JOptionPane.showInputDialog("Ingrese su nombre");
-        nom.setNombre(n);
-
-        var ape = new Persona();
         var a = JOptionPane.showInputDialog("Ingrese su apellido");
-        ape.setApellido(a);
-        var mos = new Persona();
-        mos.mostrar();
+        var nom = new Persona();
+
+        
+
     }
 }
