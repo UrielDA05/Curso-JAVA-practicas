2df2b4ef4d7393b773dd30b904b40194590668c6
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-10 21:04:11 -0600

    Ejemplo de encapsulamiento

--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -4,9 +4,10 @@ import javax.swing.JOptionPane;
 public class PruebaPersona {
     public static void main(String[] args) {
 
-        var nom = new Persona();
+        var obj1 = new Persona();
+        obj1.setNombre("Uriel");
+        obj1.setApellido(" De Jesus");
 
-
-
+        
     }
 }
