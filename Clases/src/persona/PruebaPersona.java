64498905d053b712847323495b001e8a569ac212
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-11 21:18:06 -0600

    Ejemplo de encapsulamiento

--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -7,9 +7,11 @@ public class PruebaPersona {
         var obj1 = new Persona();
         obj1.setNombre("Uriel");
         obj1.setApellido(" De Jesus");
+        obj1.mostrar();
 
         var obj2 = new Persona();
         obj2.setNombre("Gustavo");
-        obj2.setNombre("Atenogenes");
+        obj2.setApellido("Atenogenes");
+        obj2.mostrar();
     }
 }
