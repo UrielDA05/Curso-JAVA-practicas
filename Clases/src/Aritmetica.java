44980fd4b056f64c77f2362ccf3bc77fc544663b
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-12 22:05:50 -0600

    Clases y objetos

--- Clases/src/Aritmetica.java
+++ Clases/src/Aritmetica.java
@@ -24,18 +24,5 @@ public class Aritmetica {
         System.out.println("Resultado de la division: " + resultado);
     }
 
-    public static void main(String[] args) {
-        var seleccion = JOptionPane.showInputDialog("""
-                LISTA DE OPERACIONES
-                1. Suma
-                2. Resta
-                3. Multiplicacion
-                4. Division\t
-                Ingrese el numero de la operacion a realizar""");
-        switch (seleccion){
-            case 1:
 
-        }
-
-    }
 }
