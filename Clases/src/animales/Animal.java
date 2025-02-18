8e459f9109609d7e45be8bc2ed4fba189c72ebad
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-17 21:16:02 -0600

    Clases y objetos

--- Clases/src/animales/Animal.java
+++ Clases/src/animales/Animal.java
@@ -4,5 +4,9 @@ public class Animal {
     public void comer(){
         System.out.println("Come muchas veces al dia");
     }
+
+    public void dormir(){
+        System.out.println("Duermo muchas horas");
+    }
 }
 
