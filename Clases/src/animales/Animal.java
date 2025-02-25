70a7979e8250a91a5e33d88efb839b4c19c370c4
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-02-24 22:24:22 -0600

    Ejemplo de polimorfismo

--- Clases/src/animales/Animal.java
+++ Clases/src/animales/Animal.java
@@ -15,15 +15,9 @@ class Perro extends Animal {
 }
 // Clase 3
 class Gato extends Animal {
-    public void hacer_sonido() {
-        System.out.println("Puedo mauyar");
-    }
-
     @Override
-    public void dormir(){
-        System.out.println("Paso mas de 18 horas durmiendo");
-        System.out.println("Metodo de la clase padre");
-        super.dormir();
+    public void hacerSonido() {
+        System.out.println("Puedo mauyar");
     }
 }
 
@@ -33,20 +27,15 @@ class PruebaAnimal{
         System.out.println("*** Ejemplo Herencia ***");
         System.out.println("Clase Padre: soy un animal");
         var animal1 = new Animal();
-        animal1.comer();
-        animal1.dormir();
+
         //animal1.hacersonido(); // Este metodo no existe en la clase padre
 
         System.out.println("\n Clase hija, soy un perro");
         var perro1 = new Perro();
-        perro1.hacer_sonido();
-        perro1.comer();
-        perro1.dormir();
+
 
         System.out.println("\n Clase hija, soy un gato");
         var gato1 = new Gato();
-        gato1.dormir();
-        gato1.hacer_sonido();
-        gato1.comer();
+
     }
 }
