5cd1bbdaa5dec8eef0fb862b509ac30fe177bc8b
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-03-01 23:40:44 -0600

    Metodo toString

--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -4,7 +4,7 @@ import javax.swing.JOptionPane;
 public class PruebaPersona {
     public static void main(String[] args) {
         // Solicitud de datos persona 1
-        var n1 = JOptionPane.showInputDialog("Inrese su nombre");
+        var n1 = JOptionPane.showInputDialog("Ingrese su nombre");
         var a1 = JOptionPane.showInputDialog("Ingrese su apellido");
         var obj1 = new Persona();
 
