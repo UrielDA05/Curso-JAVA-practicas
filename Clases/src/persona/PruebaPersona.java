431fe2edf214af53709fc1fa94645df74b8b4666
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2025-03-30 14:32:30 -0600

    Update PruebaPersona.java
    Uso del id

--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -9,6 +9,7 @@ public class PruebaPersona {
         var n1 = JOptionPane.showInputDialog("Ingrese su nombre");
         var a1 = JOptionPane.showInputDialog("Ingrese su apellido");
         var obj1 = new Persona(n1,a1);
+        System.out.println(obj1.toString());
         obj1.setNombre(n1);
         obj1.setApellido(a1);
         System.out.println("Variable estatica despues de la creacion del objeto: " + Persona.contadorPersonas);
@@ -16,6 +17,7 @@ public class PruebaPersona {
         var n2 = JOptionPane.showInputDialog("Ingrese su nombre");
         var a2 = JOptionPane.showInputDialog("Ingrese su apellido");
         var obj2 = new Persona(n2,a2);
+        System.out.println(obj2);
         System.out.println("Variable estatica despues de la creacion del objeto: " + Persona.contadorPersonas);
 
 
